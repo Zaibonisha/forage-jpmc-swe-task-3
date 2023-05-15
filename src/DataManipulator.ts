bfa7fdc5b63c42770e36f72a7b5467965985ec6d
Zaibonisha <zsmohamed94@gmail.com>
2023-05-15 13:54:23 +0200

    changes2

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,40 +1,45 @@
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  price_abc: number,
-  price_def: number,
-  ratio: number,
-  timestamp: Date,
-  upper_bound: number,
-  lower_bound: number,
-  trigger_alert: number | undefined,
+  stock: string;
+  top_ask_price: number;
+  price_abc: number;
+  price_def: number;
+  ratio: number;
+  timestamp: Date;
+  upper_bound: number;
+  lower_bound: number;
+  trigger_alert: number | undefined;
 }
 
 export class DataManipulator {
-  static generateRow(serverRespond: ServerRespond[]): Row {
-    const priceABC = (serverRespond[0].top_ask.price + serverRespond[0].top_bid.price) / 2;
-    const priceDEF = (serverRespond[0].top_ask.price + serverRespond[1].top_ask.price) / 2;
+  static generateRow(serverResponds: ServerRespond[]): Row[] {
+    return serverResponds.map((el: ServerRespond) => ({
+      stock: el.stock,
+      top_ask_price: el.top_ask && el.top_ask.price || 0,
+      price_abc: 0,
+      price_def: 0,
+      ratio: 0,
+      timestamp: new Date(),
+      upper_bound: 0,
+      lower_bound: 0,
+      trigger_alert: undefined,
+    }));
+  }
+
+  static updateRow(row: Row, serverResponds: ServerRespond[]): Row {
+    const priceABC = (serverResponds[0].top_ask.price + serverResponds[0].top_bid.price) / 2;
+    const priceDEF = (serverResponds[1].top_ask.price + serverResponds[1].top_bid.price) / 2;
     const ratio = priceABC / priceDEF;
-
-    // Calculate the 12-month historical average ratio
-    const historicalData = serverRespond.slice(2); // Assuming historical data starts from index 2
-    const historicalRatios = historicalData.map(data => {
-      const historicalPriceABC = (data.top_ask.price + data.top_bid.price) / 2;
-      const historicalPriceDEF = (data.top_ask.price + data.top_ask.price) / 2;
-      return historicalPriceABC / historicalPriceDEF;
-    });
-    const averageRatio = historicalRatios.reduce((sum, ratio) => sum + ratio, 0) / historicalRatios.length;
-
-    const upperBound = averageRatio * 1.1;
-    const lowerBound = averageRatio * 0.9;
+    const upperBound = 1 + 0.05;
+    const lowerBound = 1 - 0.05;
 
     return {
+      ...row,
       price_abc: priceABC,
       price_def: priceDEF,
       ratio,
-      timestamp: serverRespond[0].timestamp > serverRespond[1].timestamp
-        ? serverRespond[0].timestamp
-        : serverRespond[1].timestamp,
+      timestamp: serverResponds[0].timestamp > serverResponds[1].timestamp ? serverResponds[0].timestamp : serverResponds[1].timestamp,
       upper_bound: upperBound,
       lower_bound: lowerBound,
       trigger_alert: (ratio > upperBound || ratio < lowerBound) ? ratio : undefined,
--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,8 +1,8 @@
-import React, { Component, RefObject } from 'react';
-import { Table, Schema } from '@finos/perspective';
+import React, { Component } from 'react';
+import { Table, TableData } from '@finos/perspective';
 import { ServerRespond } from './DataStreamer';
+import { DataManipulator } from './DataManipulator';
 import './Graph.css';
-import { DataManipulator, Row } from './DataManipulator';
 
 interface IProps {
   data: ServerRespond[];
@@ -10,41 +10,42 @@ interface IProps {
   load: (table: Table) => void;
 }
 
-type TableData = Record<string, (string | number | boolean | Date)[]>[];
+interface PerspectiveViewerElement extends HTMLElement {
+  load: (table: Table) => void;
+}
 
-class Graph extends Component<IProps> {
+class Graph extends Component<IProps, {}> {
   table: Table | undefined;
-  perspectiveViewerRef: RefObject<HTMLDivElement>;
 
-  constructor(props: IProps) {
-    super(props);
-    this.perspectiveViewerRef = React.createRef<HTMLDivElement>();
+  render() {
+    return React.createElement('perspective-viewer');
   }
 
   componentDidMount() {
-    const elem = this.perspectiveViewerRef.current;
+    const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
-    if (elem !== null) {
-      const schema = {
-        price_abc: 'float',
-        price_def: 'float',
-        ratio: 'float',
-        timestamp: 'date',
-        upper_bound: 'float',
-        lower_bound: 'float',
-        trigger_alert: 'float',
-      };
+    const schema = {
+      price_abc: 'float',
+      price_def: 'float',
+      ratio: 'float',
+      timestamp: 'date',
+      upper_bound: 'float',
+      lower_bound: 'float',
+      trigger_alert: 'float',
+    };
 
-      if (window.perspective && window.perspective.worker) {
-        this.table = window.perspective.worker().table(schema);
-      }
+    if (window.perspective && window.perspective.worker()) {
+      this.table = window.perspective.worker().table(schema);
+    }
 
-      if (this.table) {
-        this.props.load(this.table);
-        elem.setAttribute('view', 'y_line');
-        elem.setAttribute('row-pivots', '["timestamp"]');
-        elem.setAttribute('columns', '["ratio", "lower_bound", "upper_bound", "trigger_alert"]');
-        elem.setAttribute('aggregates', JSON.stringify({
+    if (this.table) {
+      elem.load(this.table);
+      elem.setAttribute('view', 'y_line');
+      elem.setAttribute('row-pivots', '["timestamp"]');
+      elem.setAttribute('columns', '["ratio", "lower_bound", "upper_bound", "trigger_alert"]');
+      elem.setAttribute(
+        'aggregates',
+        JSON.stringify({
           price_abc: 'avg',
           price_def: 'avg',
           ratio: 'avg',
@@ -52,35 +53,20 @@ class Graph extends Component<IProps> {
           upper_bound: 'avg',
           lower_bound: 'avg',
           trigger_alert: 'avg',
-        }));
-      }
+        })
+      );
     }
   }
 
   componentDidUpdate() {
     if (this.table) {
-      const data: Row = DataManipulator.generateRow(this.props.data);
-
-      this.table.schema().then((schema: Schema) => {
-        const schemaKeys: string[] = Object.keys(schema);
-
-        const dataFormatted: TableData = schemaKeys.reduce((formattedData, key) => {
-          const value: (string | number | boolean | Date)[] = [data[key as keyof Row] as string | number | boolean | Date];
-          formattedData.push({ [key]: value });
-          return formattedData;
-        }, [] as TableData);
-
-        this.table!.update(dataFormatted);
-      });
+      this.table.update([DataManipulator.generateRow(this.props.data)] as unknown as TableData);
     }
-  }
-
-  render() {
-    return <div ref={this.perspectiveViewerRef} />;
   }
 }
 
 export default Graph;
+
 
 
 
