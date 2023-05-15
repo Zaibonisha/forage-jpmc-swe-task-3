5ec4a729bbc2a8d0e88a05297386a82dcc7735d1
Zaibonisha <zsmohamed94@gmail.com>
2023-05-15 12:22:33 +0200

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import DataStreamer, { ServerRespond } from './DataStreamer';
 import Graph from './Graph';
 import './App.css';
+import { Table } from '@finos/perspective';
 
 interface IState {
-  data: ServerRespond[],
-  showGraph: boolean,
+  data: ServerRespond[];
+  showGraph: boolean;
 }
+
+type TableData = Record<string, (string | number | boolean | Date)[]>[];
 
 class App extends Component<{}, IState> {
   constructor(props: {}) {
@@ -15,11 +18,27 @@ class App extends Component<{}, IState> {
       data: [],
       showGraph: false,
     };
+
+    this.getDataFromServer = this.getDataFromServer.bind(this);
   }
+
+  loadTable = (table: Table) => {
+    const data = this.state.data;
+
+    const dataFormatted: TableData = data.map(row => {
+      const formattedRow: Record<string, (string | number | boolean | Date)[]> = {};
+      Object.entries(row).forEach(([key, value]) => {
+        formattedRow[key] = [value as string | number | boolean | Date];
+      });
+      return formattedRow;
+    });
+
+    table.update(dataFormatted);
+  };
 
   renderGraph() {
     if (this.state.showGraph) {
-      return (<Graph data={this.state.data}/>)
+      return <Graph data={this.state.data} color="blue" load={this.loadTable} />;
     }
   }
 
@@ -42,18 +61,26 @@ class App extends Component<{}, IState> {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          Bank Merge & Co Task 3
-        </header>
+        <header className="App-header">Bank Merge & Co Task 3</header>
         <div className="App-content">
-          <button className="btn btn-primary Stream-button" onClick={() => {this.getDataFromServer()}}>Start Streaming Data</button>
-          <div className="Graph">
-            {this.renderGraph()}
-          </div>
+          <button
+            className="btn btn-primary Stream-button"
+            onClick={this.getDataFromServer}
+          >
+            Start Streaming Data
+          </button>
+          <div className="Graph">{this.renderGraph()}</div>
         </div>
       </div>
-    )
+    );
   }
 }
 
 export default App;
+
+
+
+
+
+
+
