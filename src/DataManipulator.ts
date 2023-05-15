062878480afd1cc23f6bbeead99e0c743706973d
Zaibonisha <zsmohamed94@gmail.com>
2023-05-15 13:58:10 +0200

    changes3

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -13,39 +13,31 @@ export interface Row {
 }
 
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]): Row[] {
-    return serverResponds.map((el: ServerRespond) => ({
-      stock: el.stock,
-      top_ask_price: el.top_ask && el.top_ask.price || 0,
-      price_abc: 0,
-      price_def: 0,
-      ratio: 0,
-      timestamp: new Date(),
-      upper_bound: 0,
-      lower_bound: 0,
-      trigger_alert: undefined,
-    }));
-  }
-
-  static updateRow(row: Row, serverResponds: ServerRespond[]): Row {
+  static generateRow(serverResponds: ServerRespond[]): Row {
     const priceABC = (serverResponds[0].top_ask.price + serverResponds[0].top_bid.price) / 2;
     const priceDEF = (serverResponds[1].top_ask.price + serverResponds[1].top_bid.price) / 2;
     const ratio = priceABC / priceDEF;
     const upperBound = 1 + 0.05;
     const lowerBound = 1 - 0.05;
 
+    const timestamp = new Date(serverResponds[0].timestamp > serverResponds[1].timestamp
+      ? serverResponds[0].timestamp
+      : serverResponds[1].timestamp);
+
     return {
-      ...row,
+      stock: serverResponds[0].stock,
+      top_ask_price: serverResponds[0].top_ask && serverResponds[0].top_ask.price || 0,
       price_abc: priceABC,
       price_def: priceDEF,
       ratio,
-      timestamp: serverResponds[0].timestamp > serverResponds[1].timestamp ? serverResponds[0].timestamp : serverResponds[1].timestamp,
+      timestamp,
       upper_bound: upperBound,
       lower_bound: lowerBound,
       trigger_alert: (ratio > upperBound || ratio < lowerBound) ? ratio : undefined,
     };
   }
 }
+
 
 
 
